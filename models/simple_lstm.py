49b5b7100d1b81fb7cb6c374c792bc79539db7d8
Alexander William Wong <alex@udia.ca>
2020-03-03 12:42:37 -0700

    use lstm output instead of h_n

--- models/simple_lstm.py
+++ models/simple_lstm.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torch.nn.utils.rnn import pack_padded_sequence
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 
 class SimpleLSTM(nn.Module):
@@ -19,29 +19,46 @@ class SimpleLSTM(nn.Module):
     ):
         super(SimpleLSTM, self).__init__()
 
+        self.num_directions = int(bidirectional) + 1
+        self.hidden_size = hidden_size
+
         self.lstm1 = nn.LSTM(
             in_channels,
             hidden_size=hidden_size,
             num_layers=num_layers,
-            dropout=dropout,  # input tensors as (batch, seq, feature)
+            dropout=dropout,
             bidirectional=bidirectional,
         )
+        # output: (seq, batch, num_directions * hidden_size)
 
-        inp_size = hidden_size * num_layers
-        if bidirectional:
-            inp_size *= 2
+        inp_size = hidden_size * self.num_directions
         self.flatten = nn.Flatten()
         self.fc = nn.Linear(inp_size, num_classes)
 
     def forward(self, batch):
         x = batch["signal"]  # (seq, batch, feature)
+        total_length = x.size(0)
+        batch_size = x.size(1)
         lens = batch["len"]
         packed_x = pack_padded_sequence(x, lens, enforce_sorted=False)
 
-        _packed_out, (h_n, _c_n) = self.lstm1(packed_x)
-        # h_n = (num_layers * num_directions, batch, hidden_size)
-        h_n = torch.transpose(h_n, 0, 1)
-        h_n = self.flatten(h_n)
-        out = self.fc(h_n)
+        packed_out, _ = self.lstm1(packed_x)
+        padded_out, _ = pad_packed_sequence(packed_out, total_length=total_length)
 
+        # separate out the forward/backward if required
+        seq_batch_dir_hidden = padded_out.view(
+            total_length, batch_size, self.num_directions, self.hidden_size
+        )
+
+        # forward is 0, backward is 1
+        to_fc = []
+        for idx, length in enumerate(lens):
+            seq_dir_hidden = seq_batch_dir_hidden[:,idx]
+            out_hidden = seq_dir_hidden[length - 1, 0]
+            if self.num_directions > 1:
+                back_hidden = seq_dir_hidden[0, 1]
+                out_hidden = torch.cat((out_hidden, back_hidden))
+            to_fc.append(out_hidden)
+        to_fc = torch.stack(to_fc)
+        out = self.fc(to_fc)
         return out
