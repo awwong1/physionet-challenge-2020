2f0290f3a47eaccca02df5d735a08fd3933fa063
Alexander Wong <alex@udia.ca>
2020-03-29 09:48:40 -0600

    trying pooling for proc_extract_features

--- agents/feature_extraction.py
+++ agents/feature_extraction.py
@@ -1,12 +1,14 @@
 import os
 from collections import OrderedDict
+from functools import partial
+from multiprocessing import Pool
 
 import numpy as np
 from tqdm import tqdm
 
 from driver import get_classes, load_challenge_data
-from util.sigproc import extract_record_features
 from util.sigdat import convert_to_wfdb_record, extract_features
+from util.sigproc import extract_record_features
 
 from . import BaseAgent
 
@@ -34,35 +36,56 @@ class FeatureExtractionAgent(BaseAgent):
         self.input_files = tuple(sorted(input_files))
         self.tqdm = tqdm(self.input_files, desc=f"Extracting {self.input_directory}")
 
-    def run(self):
-        for input_file in self.tqdm:
-            fp = os.path.join(self.input_directory, input_file)
-            data, headers = load_challenge_data(fp)
+    @staticmethod
+    def proc_extract_features(input_file, input_dir="", output_dir=""):
+        fp = os.path.join(input_dir, input_file)
+        data, headers = load_challenge_data(fp)
 
-            # features = extract_record_features(data, headers, **self.erf_options)
-            r = convert_to_wfdb_record(data, headers)
-            features = extract_features(r)
+        # features = extract_record_features(data, headers, **self.erf_options)
+        r = convert_to_wfdb_record(data, headers)
+        features = extract_features(r)
 
-            sex = features.pop("sex")
-            age = features.pop("age")
-            target = features.pop("target")
+        sex = features.pop("sex")
+        age = features.pop("age")
+        target = features.pop("target")
 
-            sig = features.pop("sig")
-            # split out each lead as as its own feature array
-            np_savez_data = OrderedDict()
-            for sig_name, sig_features in sig.items():
-                np_savez_data[sig_name] = np.concatenate([
+        sig = features.pop("sig")
+        # split out each lead as as its own feature array
+        np_savez_data = OrderedDict()
+        for sig_name, sig_features in sig.items():
+            np_savez_data[sig_name] = np.concatenate(
+                [
                     sex.flatten(),
                     age.flatten(),
-                    *[v.flatten() for v in sig_features.values()]
-                ])
+                    *[v.flatten() for v in sig_features.values()],
+                ]
+            )
 
-            # assert len(inputs) == 4479, f"Input length not consistent on {input_file}"
+        # assert len(inputs) == 4479, f"Input length not consistent on {input_file}"
 
-            bn = os.path.splitext(input_file)[0]
-            out_f = os.path.join(self.out_dir, f"{bn}.npz")
+        bn = os.path.splitext(input_file)[0]
+        out_f = os.path.join(output_dir, f"{bn}.npz")
 
-            np.savez(out_f, target=target, **np_savez_data)
+        np.savez(out_f, target=target, **np_savez_data)
+
+
+    def run(self):
+        try:
+            procs = len(os.sched_getaffinity(0))
+        except:
+            procs = 1
+
+        with Pool(procs, initializer=tqdm.set_lock, initargs=(tqdm.get_lock(),)) as p:
+            list(
+                p.imap_unordered(
+                    partial(
+                        FeatureExtractionAgent.proc_extract_features,
+                        input_dir=self.input_directory,
+                        output_dir=self.out_dir,
+                    ),
+                    self.tqdm,
+                )
+            )
 
     def finalize(self):
         if self.tqdm:
