dede14ad5a8dc21d0918a56ea9e1222e49f8ac63
Alexander William Wong <alex.wong@ualberta.ca>
2020-08-05 16:40:50 -0600

    map for iteration speedup (hopefully)

--- run_12ECG_classifier.py
+++ run_12ECG_classifier.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import functools
 import os
 
 import joblib
@@ -13,18 +14,36 @@ def run_12ECG_classifier(data, header_data, loaded_model):
     r = convert_to_wfdb_record(data, header_data)
     record_features, dx = wfdb_record_to_feature_dataframe(r)
 
-    classes = []
-    labels = []
-    scores = []
-    for k, v in loaded_model.items():
-        if not is_number(k):
-            continue
-        classes.append(str(k))
-        labels.append(int(v.predict(record_features)[0]))
-        scores.append(v.predict_proba(record_features)[0][1])
+    featured_classifier_run = functools.partial(
+        _partial_run_classifier, record_features=record_features
+    )
+
+    # parallel
+    # output = joblib.Parallel(n_jobs=-1, verbose=0)(
+    #     joblib.delayed(featured_classifier_run)(kv)
+    #     for kv in loaded_model.items()
+    #     if is_number(kv[0])
+    # )
+
+    # map
+    output = map(
+        featured_classifier_run, [kv for kv in loaded_model.items() if is_number(kv[0])]
+    )
+
+    labels, scores, classes = zip(*output)
+
+    # force labels to be integers
+    labels = list(map(int, labels))
 
     # return current_label, current_score, classes
     return labels, scores, classes
+
+
+def _partial_run_classifier(kv, record_features=None):
+    class_val, model = kv
+    label = model.predict(record_features)[0]
+    score = model.predict_proba(record_features)[0][1]
+    return label, score, str(class_val)
 
 
 def load_12ECG_model(input_directory):
@@ -33,7 +52,7 @@ def load_12ECG_model(input_directory):
 
     print(f"Loading {model_fps[-1]} from {input_directory}...")
 
-    filename = os.path.join(input_directory, model_fps[0])
+    filename = os.path.join(input_directory, model_fps[-1])
 
     loaded_model = joblib.load(filename)
 
